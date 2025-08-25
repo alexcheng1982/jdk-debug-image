6f3a6f9cd91d11ad1a1123cfbdb9f3b40296b1af
Fu Cheng <alexcheng1982@gmail.com>
2025-08-26 11:12:26 +1200

    update entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,4 @@ set -e
 
 ulimit -c unlimited
 
-JAVA_OPTS="${JAVA_OPTS:-}"
-
-if [ "${1#-}" != "$1" ]; then
-  exec java $JAVA_OPTS "$@"
-fi
-
-if [ "$1" = "java" ]; then
-  shift
-  exec java $JAVA_OPTS "$@"
-fi
-
 exec "$@"
